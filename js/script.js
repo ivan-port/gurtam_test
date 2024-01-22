379b6bb0a855d6ecd5bfbe33d03f2bb8dbfc2700
Ivan <Ivan.portsiankov@gmail.com>
2024-01-22 22:42:32 +0200

    php ссылка

--- js/script.js
+++ js/script.js
@@ -291,7 +291,7 @@ document.addEventListener('DOMContentLoaded',function(){
 
       if(error===0){
             element.classList.add('_sending');/**картинка с загрузкой */
-          let response=await fetch('https://github.com/ivan-port/gurtam_test/blob/main/smart.php',{
+          let response=await fetch('https://github.com/ivan-port/gurtam_test/smart.php',{
               method:'POST',
               body:formData
           });
